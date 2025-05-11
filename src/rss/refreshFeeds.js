fdd89c49fc5f578ad033da8f5339a9cc7411f63b
Anakharsis9 <dayana530807@gmail.com>
2025-05-11 22:38:01 +0500

    fix: eslint errors

--- src/rss/refreshFeeds.js
+++ src/rss/refreshFeeds.js
@@ -2,15 +2,15 @@ import fetchRss from './fetchRss.js'
 import normalizeData from './normalize.js'
 
 export default function refreshFeeds(state) {
-  const tasks = state.feeds.map(feed => 
+  const tasks = state.feeds.map(feed =>
     fetchRss(feed.url)
-      .then(rawData => {
+      .then((rawData) => {
         const { posts } = normalizeData(rawData, feed.url, feed.id)
         const existingLinks = state.posts.map(post => post.link)
         const newPosts = posts.filter(post => !existingLinks.includes(post.link))
 
         if (newPosts.length > 0) {
-          state.posts = [...newPosts, ...state.posts]
+          state.posts = [...newPosts, ...state.posts,]
         }
       })
       .catch(() => {})
